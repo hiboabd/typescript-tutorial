32518dda258e665f880d7c0e581cfc0a58e6572e
hiboabd <51047911+hiboabd@users.noreply.github.com>
2020-08-28 08:21:21 +0100

    Exploration of the string, number and boolean types

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2; // this prevents string concatenation of line 4 by calculating the result before adding it to the phrase
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 var number1 = 5;
-var number2 = '2.8';
-var result = add(number1, number2);
-console.log(result);
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
+// console.log(result); - dont need to console log anymore as the function has output functionality
