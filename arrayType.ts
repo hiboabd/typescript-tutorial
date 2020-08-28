16f709b76c68cf3964ca3d61ebb6c61ac95e30d9
hiboabd <51047911+hiboabd@users.noreply.github.com>
2020-08-28 09:10:14 +0100

    Exploring the array type

---
+++ arrayType.ts
@@ -0,0 +1,16 @@
+const person2 = {
+    age: 30,
+    name: 'Maximillian',
+    hobbies: ['Sports', 'Cooking']
+}
+
+let favouriteActivities : string[]; // array of strings
+let favouriteActivities2 : any[]; // array of any type
+favouriteActivities = ['Sports'. 1]
+
+console.log(person2.name)
+
+for (const hobby of person2.hobbies){
+    console.log(hobby.toUpperCase())
+    // console.log(hobby.map()) Error!! As map is not a method of type string
+}
