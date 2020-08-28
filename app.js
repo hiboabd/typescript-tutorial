4d627b566f9e25cde83349f7bfb69b7d42a96eb1
hiboabd <51047911+hiboabd@users.noreply.github.com>
2020-08-28 08:45:02 +0100

    Exploring the object type

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2; // this prevents string concatenation of line 4 by calculating the result before adding it to the phrase
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-var result = add(number1, number2, printResult, resultPhrase);
-// console.log(result); - dont need to console log anymore as the function has output functionality
+var person = {
+    age: 30,
+    name: 'Maximillian'
+};
+console.log(person.name);
+// this is a very explicit method and not best practise, it is better to let typescript infer the types
+// const person: {
+//     name: string;
+//     age: number
+// } = {
+//     age: 30,
+//     name: 'Maximillian'
+// }
+// You can specify that the variable is of type object which is generic
+// however this does not specify the properties that it can hold so even though a property exists we don't give any other information 
+// to typescript so it doesnt support any properties
+// const person: object = {
+//     age: 30,
+//     name: 'Maximillian'
+// }
+// assigning an empty curly brace is the same as typing object
